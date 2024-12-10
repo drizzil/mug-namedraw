7b3a5274195af8fc9ea552d8b36362898d8f5e1e
Josh Newsome <jn799421@gmail.com>
2024-12-09 23:16:35 -0500

    changes to redirect

--- js/start.js
+++ js/start.js
@@ -2,3 +2,8 @@ window.onload = function() {
     alert('Page is loaded and script is running!');
     console.log('This is a message from the JavaScript file.');
   };
+
+  if (window.location.pathname === '/') {
+    // Redirect to 'index.html'
+    window.location.pathname = '/index.html';
+  }
