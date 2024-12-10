e5dfd819709db3a0045b22546dbb993ab9f72c9d
Josh Newsome <jn799421@gmail.com>
2024-12-10 08:51:26 -0500

    changes

--- api/getData.js
+++ api/getData.js
@@ -13,7 +13,7 @@ const pool = new Pool({
 export default async function handler(req, res) {
   try {
     // Query the database
-    const result = await pool.query('SELECT * FROM nameSelection');
+    const result = await pool.query('SELECT * FROM nameselection');
     res.status(200).json(result.rows);
   } catch (error) {
     console.error('Error fetching data from database:', error);
